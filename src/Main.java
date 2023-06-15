211fa61b984348543f7f0fe9d7b04fc1e163b588
okamodgi <nadakupnada@gmail.com>
2023-06-15 14:40:59 +0300

    laboratornaya13

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,21 @@ public class Main {
         System.out.println("Введите IP-адрес: ");
         String ipAddress = scanner.nextLine();
 
-        String regex = "((\\d|\\d\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3}(25[0-5]|2[0-4]\\d|1\\d\\d|\\d\\d|\\d)";
+        String regex = "^((\\d|\\d\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3}(25[0-5]|2[0-4]\\d|1\\d\\d|\\d\\d|\\d)$";
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(ipAddress);
 
         try {
             FileWriter writer = new FileWriter("ip_addresses.txt", true); // открываем файл для записи
-            if (matcher.find()) {                        //Поиск в строке str соответствий с помощью метода find() объекта matcher.
+            if (matcher.find()) {                                                        //Поиск в строке str соответствий с помощью метода find() объекта matcher.
                 writer.write(ipAddress + " соответствует шаблону IP-адреса\n");
                 System.out.println(matcher.group() + " соответствует шаблону IP-адреса");
             } else {
                 writer.write(ipAddress + " не соответствует шаблону IP-адреса\n");
                 System.out.println(ipAddress + " не соответствует шаблону IP-адреса");
             }
-            writer.close();                                                                  // закрываем файл
+            writer.close();                                                              // закрываем файл
         } catch (IOException e) {
             System.out.println("Ошибка записи в файл: " + e.getMessage());
         }
